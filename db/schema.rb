3f038e0b449da76be5342c98499440f37a6e8d24
charvine <crossovermeister@gmail.com>
2017-03-18 21:38:40 +0800

    add admin CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170318074343) do
+ActiveRecord::Schema.define(version: 20170318124712) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "video"
+    t.text     "intro"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
