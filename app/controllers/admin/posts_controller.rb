a4e7aa977bf1516c5c1e9008e832f884fe802d2e
charvine <crossovermeister@gmail.com>
2017-03-18 22:13:03 +0800

    admin access

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,5 +1,10 @@
 class Admin::PostsController < ApplicationController
 
+  before_action :authenticate_user!
+  before_action :admin_required
+
+  layout "admin"
+
  def index
    @posts = Post.all
  end
--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,4 @@
 class HomepageController < ApplicationController
   def index
-    flash[:notice] = "Welcome! Let's read together!"
   end
 end
